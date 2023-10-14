dd446b676846d9dfba65feeb71bd2a5cdc50602d
Karthik S <karthikskaranth@gmail.com>
2023-10-14 16:32:36 +0530

    Some Minor changes
    Still memory allocation is not continuous, as allocation is done for consecutive memory blocks, so finding a solution for making allocation for continuous allocation.

--- Pipelining/Methord_2/Pipeline_with_Diff_clk.v
+++ Pipelining/Methord_2/Pipeline_with_Diff_clk.v
@@ -5,27 +5,27 @@ input [7:0] addr;
 input CLK_1, CLK_2;
 output [15:0] Zout;
 
-reg [15:0] l12_a, l12_b, l23_z, l34_z;
-reg [7:0] l12_addr, l23_addr, l34_addr;
-reg [3:0] l12_rd, l12_func, l23_rd;
+reg [15:0] l12_a, l12_b, l23_z, l34_z;             // introducing latches in the pipeline
+reg [7:0] l12_addr, l23_addr, l34_addr;            // introducing latches in the pipeline
+reg [3:0] l12_rd, l12_func, l23_rd;                // introducing latches in the pipeline
 
-reg [15:0] regBank[0:15];
-reg [15:0] mem[0:255];
+reg [15:0] regBank[0:15];                          // 16 registers
+reg [15:0] mem[0:255];                             // 256 memory locations
 
-assign Zout = l34_z;
+assign Zout = l34_z;                               // output of the pipeline
 
 always @(posedge CLK_1)
 begin
-    l12_a <= #2 regBank[rs1];
+    l12_a <= #2 regBank[rs1];                      // ---------------------> STAGE 1
     l12_b <= #2 regBank[rs2];
     l12_rd <= #2 rd;
     l12_func <= #2 func;
     l12_addr <= #2 addr;
 end
 
-always @(negedge CLK_2)
+always @(posedge CLK_2)
 begin
-    l23_rd <= #2 l12_rd;
+    l23_rd <= #2 l12_rd;                          // ---------------------> STAGE 2
     l23_addr <= #2 l12_addr;
 
     case(l12_func)
@@ -47,13 +47,13 @@ end
 
 always @(posedge CLK_1)
 begin
-    regBank[l23_rd] <= #2 l23_z;
+    regBank[l23_rd] <= #2 l23_z;                       // ---------------------> STAGE 3
     l34_addr <= #2 l23_addr;
     l34_z <= #2 l23_z;
 end
 
-always @(negedge CLK_2)
+always @(posedge CLK_2)
 begin 
-    mem[l34_addr] <= #2 l34_z;
+    mem[l34_addr] <= #2 l34_z;                          // ---------------------> STAGE 4
 end
 endmodule
--- Pipelining/Methord_2/Test_pipeline_2.v
+++ Pipelining/Methord_2/Test_pipeline_2.v
@@ -7,7 +7,7 @@ reg [3:0] rs1, rs2, rd, func;
 reg [7:0] addr;
 reg CLK_1, CLK_2;
 wire [15:0] Zout;
-integer i;
+integer i, k;
 
 pipelining_2 MYPIPE(Zout, rs1, rs2, rd, func, addr, CLK_1, CLK_2);
 
@@ -23,32 +23,35 @@ end
 
 initial 
 begin 
-    for(i=0; i<16; i=i+1)
-    MYPIPE.regBank[i] = i;
+    for(i=0; i<16; i=i+1)                // initialize registers
+    MYPIPE.regBank[i] = i;               // all the registers have values from 0 to 15
 end
+
 
 initial
 begin
-    #5 rs1 = 3; rs2 = 5; rd = 10; func = 10; addr = 10;
-    #10 rs1 = 2; rs2 = 3; rd = 1; func = 1; addr = 1;
+    #5 rs1 = 3; rs2 = 5; rd = 10; func = 10; addr = 10;          // Filling the pipeline with instructions
+    #10 rs1 = 5; rs2 = 3; rd = 1; func = 1; addr = 1;
     #10 rs1 = 1; rs2 = 2; rd = 2; func = 2; addr = 2;
-    #10 rs1 = 0; rs2 = 1; rd = 3; func = 3; addr = 3;
+    #10 rs1 = 10; rs2 = 1; rd = 3; func = 3; addr = 3;
     #10 rs1 = 15; rs2 = 14; rd = 4; func = 4; addr = 4;
-    #10 rs1 = 14; rs2 = 13; rd = 5; func = 5; addr = 5;
+   #10 rs1 = 14; rs2 = 13; rd = 5; func = 5; addr = 5;
     #10 rs1 = 13; rs2 = 12; rd = 6; func = 6; addr = 6;
-    #10 rs1 = 12; rs2 = 11; rd = 7; func = 7; addr = 7;
-    #10 rs1 = 11; rs2 = 10; rd = 8; func = 8; addr = 8;
-    #10 rs1 = 10; rs2 = 9; rd = 9; func = 9; addr = 9;
+   // #10 rs1 = 12; rs2 = 11; rd = 7; func = 7; addr = 7;
+ //  #10 rs1 = 11; rs2 = 10; rd = 8; func = 8; addr = 8;
+  //  #10 rs1 = 10; rs2 = 9; rd = 9; func = 9; addr = 9; */
 
-#60  for( i=0; i<12; i=i+1 )
-      $display("mem[%3d] = %d", i, MYPIPE.mem[i]);
+#60  for( i=0; i<16; i=i+1 )
+      $display("mem[%3d] = %d", i, MYPIPE.mem[i]);              // display the contents of memory
+      for(k=0; k<16; k=k+1)
+        $display("regBank[%3d] = %d", k, MYPIPE.regBank[k]);      // display the contents of registers
 end
 
 initial 
 begin
     $dumpfile("Test_pipeline.vcd");
     $dumpvars(0, Test_pipeline);
-    $monitor("Time:%3d, Zout=%d",$time, Zout);
+    $monitor("Time:%3d, Zout=%d",$time, Zout);                  // monitor the output of the pipeline
 end
 
 endmodule
